6ca6f832268a8dedd11fc8c1b2f97fd8b16f46f4
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-08-19 23:34:34 +0700

    feat : add file main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/habibmrizki/minitas-day1/soal1"
+	"github.com/habibmrizki/minitas-day1/soal2"
+	"github.com/habibmrizki/minitas-day1/soal3"
+	"github.com/habibmrizki/minitas-day1/soal4"
+	"github.com/habibmrizki/minitas-day1/soal5"
+)
+
+func main() {
+	fmt.Println("Jawaban dari Minitask Day 1")
+	fmt.Println("===================================")
+	fmt.Println("Jawaban soal No.1")
+	soal1.Main()
+	fmt.Printf("\n")
+	fmt.Println("===================================")
+	fmt.Println("Jawaban soal No.2")
+	soal2.Main()
+	fmt.Printf("\n")
+	fmt.Println("===================================")
+	fmt.Println("Jawaban soal No.3")
+	soal3.Main()
+	fmt.Printf("\n")
+	fmt.Println("===================================")
+	fmt.Println("Jawaban soal No.4")
+	soal4.Main()
+	fmt.Printf("\n")
+	fmt.Println("===================================")
+	fmt.Println("Jawaban soal No.5")
+	soal5.Main()
+}
